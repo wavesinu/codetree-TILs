13a3cf0af7705f88a9e9e50111b452afe77e0347
ISU <wavesinu@gmail.com>
2024-02-02 23:13:18 +0900

    Title: 계단 오르기, Time: 114ms, Memory: 24MB, Status: Passed - CodeTree

--- 240202/계단 오르기/climbing-stairs.py
+++ 240202/계단 오르기/climbing-stairs.py
@@ -1,21 +1,15 @@
-def countWays(n):
-    if n == 0:
-        return 1
-    elif n == 1:
-        return 0
-    elif n == 2:
-        return 1
-    elif n == 3:
-        return 1
-    
-    DP = [0] * (n + 1)
-    DP[0], DP[1], DP[2], DP[3] = 1, 0, 1, 1
-    
-    for i in range(4, n + 1):
-        DP[i] = DP[i-2] + DP[i-3]
-    
-    return DP[n]
+MAX_N = 1000
+MOD = 10007
 
-# 예시
 n = int(input())
-print(countWays(n))
+dp = [0] * (MAX_N + 1)
+
+dp[0] = 1
+dp[1] = 0
+dp[2] = 1
+dp[3] = 1
+
+for i in range(4, n + 1):
+    dp[i] = (dp[i - 2] + dp[i - 3]) % MOD
+
+print(dp[n])
